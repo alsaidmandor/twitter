0589a0df39d19f8086b2cf0b9b5050bde0746401
alsaidmandor <alsaidmandor8888@gmail.com>
2023-03-07 18:22:29 +0200

    init

---
+++ android/app/src/main/kotlin/com/soft/ss/twitter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.soft.ss.twitter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String databaseId = '64075a2531d32e9765fa';
+  static const String projectId = '640757c807348aed3812';
+  static const String endPoint = 'http://localhost:8/v1';
+}
